c80bb6d4702a465efb61cf09c5b6dba3025007c0
Emins_Mac <eminskuo@gmail.com>
2024-11-16 08:05:05 +0800

    2024-11-16 Update Multilayer Perceptron & my_lib.py(fashion mnist data loader function & trainer_ch3 function)

--- my_lib.py
+++ my_lib.py
@@ -52,7 +52,7 @@ def input_data_process(features, labels):
     输入数据处理函数
     把正常的数据集转化为torch.utils.data.Dataset类型
     输入的数据集格式必须为：torch.Tensor类型；numpy.ndarray类型；list类型
-    输入结果为：torch.utils.data.Dataset类型
+    输出结果为：torch.utils.data.Dataset类型
     """
     class MyDataSet(torch.utils.data.Dataset):
         def __init__(self, features, labels):
@@ -183,3 +183,167 @@ def softmax_regression(features_train, labels_train, features_test, labels_test,
     print("\n开始测试...")
     test_model()   # 测试模型
 
+#####################################################################################################################################
+
+
+
+
+
+
+#########################DIY Pytorch train_ch3 function & Fashion-MNIST data load function############################################
+import torch
+from IPython import display
+from d2l import torch as d2l
+
+
+
+def accuracy(y_hat, y):  #@save
+    """计算预测正确的数量"""
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = y_hat.argmax(axis=1)
+    cmp = y_hat.type(y.dtype) == y
+    return float(cmp.type(y.dtype).sum())
+
+
+def evaluate_accuracy(net, data_iter):  #@save
+    """计算在指定数据集上模型的精度"""
+    if isinstance(net, torch.nn.Module):
+        net.eval()  # 将模型设置为评估模式
+    metric = Accumulator(2)  # 正确预测数、预测总数
+    with torch.no_grad():
+        for X, y in data_iter:
+            metric.add(accuracy(net(X), y), y.numel())
+    return metric[0] / metric[1]
+
+
+
+class Accumulator:  #@save
+    """在n个变量上累加"""
+    def __init__(self, n):
+        self.data = [0.0] * n
+
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+
+    def reset(self):
+        self.data = [0.0] * len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+
+def train_epoch_ch3(net, train_iter, loss, updater):  #@save
+    """训练模型一个迭代周期（定义见第3章）"""
+    # 将模型设置为训练模式
+    if isinstance(net, torch.nn.Module):
+        net.train()
+    # 训练损失总和、训练准确度总和、样本数
+    metric = Accumulator(3)
+    for X, y in train_iter:
+        # 计算梯度并更新参数
+        y_hat = net(X)
+        l = loss(y_hat, y)
+        if isinstance(updater, torch.optim.Optimizer):
+            # 使用PyTorch内置的优化器和损失函数
+            updater.zero_grad()
+            l.mean().backward()
+            updater.step()
+        else:
+            # 使用定制的优化器和损失函数
+            l.sum().backward()
+            updater(X.shape[0])
+        metric.add(float(l.sum()), accuracy(y_hat, y), y.numel())
+    # 返回训练损失和训练精度
+    return metric[0] / metric[2], metric[1] / metric[2]
+
+
+
+
+class Animator:  #@save
+    """在动画中绘制数据"""
+    def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None,
+                 ylim=None, xscale='linear', yscale='linear',
+                 fmts=('-', 'm--', 'g-.', 'r:'), nrows=1, ncols=1,
+                 figsize=(3.5, 2.5)):
+        # 增量地绘制多条线
+        if legend is None:
+            legend = []
+        d2l.use_svg_display()
+        self.fig, self.axes = d2l.plt.subplots(nrows, ncols, figsize=figsize)
+        if nrows * ncols == 1:
+            self.axes = [self.axes, ]
+        # 使用lambda函数捕获参数
+        self.config_axes = lambda: d2l.set_axes(
+            self.axes[0], xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+        self.X, self.Y, self.fmts = None, None, fmts
+
+    def add(self, x, y):
+        # 向图表中添加多个数据点
+        if not hasattr(y, "__len__"):
+            y = [y]
+        n = len(y)
+        if not hasattr(x, "__len__"):
+            x = [x] * n
+        if not self.X:
+            self.X = [[] for _ in range(n)]
+        if not self.Y:
+            self.Y = [[] for _ in range(n)]
+        for i, (a, b) in enumerate(zip(x, y)):
+            if a is not None and b is not None:
+                self.X[i].append(a)
+                self.Y[i].append(b)
+        self.axes[0].cla()
+        for x, y, fmt in zip(self.X, self.Y, self.fmts):
+            self.axes[0].plot(x, y, fmt)
+        self.config_axes()
+        display.display(self.fig)
+        display.clear_output(wait=True)
+
+
+
+
+def train_ch3(net, train_iter, test_iter, loss, num_epochs, updater):  #@save
+    """训练模型（定义见第3章）"""
+    animator = Animator(xlabel='epoch', xlim=[1, num_epochs], ylim=[0.3, 0.9],
+                        legend=['train loss', 'train acc', 'test acc'])
+    for epoch in range(num_epochs):
+        train_metrics = train_epoch_ch3(net, train_iter, loss, updater)
+        test_acc = evaluate_accuracy(net, test_iter)
+        animator.add(epoch + 1, train_metrics + (test_acc,))
+    train_loss, train_acc = train_metrics
+    assert train_loss < 0.5, train_loss
+    assert train_acc <= 1 and train_acc > 0.7, train_acc
+    assert test_acc <= 1 and test_acc > 0.7, test_acc
+
+from torchvision import transforms
+import torchvision
+
+
+def load_data_fashion_mnist(batch_size, resize=None):  #@save
+    """下载Fashion-MNIST数据集，然后将其加载到内存中"""
+    transform = transforms.Compose([
+    transforms.ToTensor(),  # 将PIL图像或numpy.ndarray转换为tensor，并归一化到[0,1]
+    transforms.Normalize((0.5,), (0.5,))  # Fashion-MNIST的标准化参数
+    #这是对图像数据进行标准化（归一化）处理的操作
+    # 标准化处理：将图像数据转换为均值为0.5，标准差为0.5的标准正态分布
+    ])
+    # 加载Fashion-MNIST训练数据集
+    mnist_train = torchvision.datasets.FashionMNIST(
+        root='./data',      # 数据存储路径
+        train=True,         # 指定为训练集
+        transform=transform,  # 应用上面定义的预处理
+        download=True       # 如果数据不存在则下载
+        )
+
+    # 加载Fashion-MNIST测试数据集
+    mnist_test = torchvision.datasets.FashionMNIST(
+        root='./data',      # 数据存储路径
+        train=False,        # 指定为测试集
+        transform=transform  # 应用相同的预处理
+        )
+    return (data.DataLoader(mnist_train, batch_size, shuffle=True,
+                            num_workers=4),
+            data.DataLoader(mnist_test, batch_size, shuffle=False,
+                            num_workers=4))
+
+#########################################################################################################################
